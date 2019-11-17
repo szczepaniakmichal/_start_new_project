c5def198f0992024425a481b7f1199cbad06340a
szczepaniakmichal <mssk87@gmail.com>
2019-11-17 16:37:43 +0100

    gulp error = The following tasks did not complete: sass
                 Did you forget to signal async completion?
    
                 answer: in this code/task/commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function () {
+gulp.task('sass', done => {
    gulp.src('sass/style.scss')
        .pipe(sass())
-       .pipe(gulp.dest('css/'))
+       .pipe(gulp.dest('css/'));
+   done();
 });
